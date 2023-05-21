cb8958c061ae95fee289e2149128c2160e321b17
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2023-05-21 17:28:30 +0800

    started analysis portion

--- website/views.py
+++ website/views.py
@@ -2,6 +2,12 @@ from flask import Blueprint, render_template, flash, request, redirect, url_for
 from flask_login import login_required, current_user
 from .models import User
 from . import db
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+
 
 views = Blueprint('views', __name__)
 
@@ -9,4 +15,20 @@ views = Blueprint('views', __name__)
 @views.route('/home')
 @login_required
 def home():
-    return render_template('base.html', user=current_user)
+    yfdata = yf.download('BTC-USD')
+    yfdata.to_csv('btcprice.csv')
+    btc_price_data = pd.read_csv('btcprice.csv',
+                             index_col=False, 
+                             low_memory=False,  
+                             names=["Date", 
+                                    "Open",
+                                    "High",
+                                    "Low",
+                                    "Close",
+                                    "Adj Close",
+                                    "Volume"],
+                                    skiprows=1)
+    
+
+
+    return render_template('home.html', user=current_user)
